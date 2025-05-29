abd05e333478fe2771410d97468becedc00f65da
Rashi Chandra <rashi.chandra@ukg.com>
2025-05-29 21:14:14 +0530

    API that converts any document to PDF

---
+++ src/main/java/com/automated/pdfconversion/PdfconversionApplication.java
@@ -0,0 +1,13 @@
+package com.automated.pdfconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfconversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfconversionApplication.class, args);
+	}
+
+}
